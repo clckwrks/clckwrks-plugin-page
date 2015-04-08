95e8e100cbc58111001d037e79da018c1bf1077c
Jeremy Shaw <jeremy@n-heptane.com>
2015-04-08 11:43:46 -0500

    0.3.8 - added .nix files. bumped build depends.

---
+++ default.nix
@@ -0,0 +1,23 @@
+{ mkDerivation, acid-state, aeson, attoparsec, base, clckwrks
+, containers, directory, filepath, happstack-hsp, happstack-server
+, hsp, hsx2hs, ixset, mtl, old-locale, random, reform
+, reform-happstack, reform-hsp, safecopy, stdenv, tagsoup
+, template-haskell, text, time, uuid, web-plugins, web-routes
+, web-routes-happstack, web-routes-th
+}:
+mkDerivation {
+  pname = "clckwrks-plugin-page";
+  version = "0.3.8";
+  src = ./.;
+  buildDepends = [
+    acid-state aeson attoparsec base clckwrks containers directory
+    filepath happstack-hsp happstack-server hsp hsx2hs ixset mtl
+    old-locale random reform reform-happstack reform-hsp safecopy
+    tagsoup template-haskell text time uuid web-plugins web-routes
+    web-routes-happstack web-routes-th
+  ];
+  buildTools = [ hsx2hs ];
+  homepage = "http://www.clckwrks.com/";
+  description = "support for CMS/Blogging in clckwrks";
+  license = stdenv.lib.licenses.bsd3;
+}
